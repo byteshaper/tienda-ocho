8ecd239a8d2e0b434a17723e5e315d07ec89f69e
Henning Schütz <henning@byteshaper.de>
2018-10-27 15:12:57 +0200

    initial SpringBoot project and some doc

---
+++ t8-backend/src/main/kotlin/com/byteshaper/tienda8/Tienda8Application.kt
@@ -0,0 +1,11 @@
+package com.byteshaper.tienda8
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Tienda8Application
+
+fun main(args: Array<String>) {
+    runApplication<Tienda8Application>(*args)
+}
---
+++ t8-backend/src/test/kotlin/com/byteshaper/tienda8/Tienda8ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.byteshaper.tienda8
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Tienda8ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
